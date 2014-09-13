e871ab0ae5738edfc954585bdb5422bd176b1ee2
Joseph Malone <joseph@josephmalone.com>
2014-09-13 10:16:37 -0500

    Update sendEmailNotification.js

--- sendEmailNotification.js
+++ sendEmailNotification.js
@@ -14,7 +14,7 @@ function sendEmailNotification() {
     var message = "";
     var signature = "";
 
-    var label = GmailApp.getUserLabelByName('emailNotifications'); 
+    var label = GmailApp.getUserLabelByName('sendEmailNotification'); 
     var threads = label.getThreads();
     for (var i = 0; i < threads.length; i++) {
        var originalSubject = threads[i].getFirstMessageSubject();
@@ -51,7 +51,7 @@ function stopsmsalerts() {
 // 3. Click on “Settings” link from the dropdown.
 // 4. Now, from the newly loaded page, click on “Labels” menu.
 // 5. Scroll to the bottom of the page, click on “Create new label”.
-// 6. Name it exactly “emailNotifications” and the click on “Create” button from the popup box. The popup should close now.
+// 6. Name it exactly “sendEmailNotification” and the click on “Create” button from the popup box. The popup should close now.
 // 7. Now, from the same menu on which you clicked “Labels”, it’s time to click on “Filters”.
 // Now, we will filter the type of email notifications you want to receive. Like, you want to receive only those mails which contain name “Fred,” such filtering can be done here. 
 // 8. Again scroll down and reach bottom of the page.
@@ -60,7 +60,7 @@ function stopsmsalerts() {
 // 11. After you have done with filter contents, click on “Create filter with this search >>” which is at the bottom of the dropdown window.
 // 12. Now, the dropdown contents should get updated.
 // 13. Checkmark the label saying “Apply the Label”.
-// 14. Now, from the combo box in front of the label, select the label with exact name as “emailNotifications”.
+// 14. Now, from the combo box in front of the label, select the label with exact name as “sendEmailNotification”.
 // 15. Now, click on the blue button in the same dropdown which says “Create Filter”. The filter should be created by now.
 // ———————————— Settings in Google Doc ————————————
 // 1. Now, go to google docs from the same logged-in account. If you can’t find the link, then follow here: https://docs.google.com/‎
@@ -70,7 +70,7 @@ function stopsmsalerts() {
 // 5. You will be redirected to a new page for writing some code. Don’t worry, I will provide you the code for it :)
 // 6. In the code tab named as “Code.gs”, select the prewritten code and brutally delete it. Blank the whole window and color it from the white blood of codes.
 // 7. For the code, use what’s here.
-// 8. Now, from the window menu, click on “File” and save the file with name exactly “emailNotifications”.
+// 8. Now, from the window menu, click on “File” and save the file with name exactly “sendEmailNotification”.
 // 9. Now, from the same window menu, click on “Run” and from the dropdown select “authorize”.
 // 10. Now, close this window and go to the spreadsheet. Close the spreadsheet and reopen it. You will be able to see a new menu as “Get Email Alerts”.
 // 11. Click on it and from the dropdown select “Authorize”.
